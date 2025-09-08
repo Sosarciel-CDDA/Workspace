e818fac65430f996cb28ffd4cc71936f57eb7808
zwa73 <ssfen2419@126.com>
2025-09-09 02:31:55 +0800

    fx

--- UniqueNPC/src/UNDefine.ts
+++ UniqueNPC/src/UNDefine.ts
@@ -1,11 +1,5 @@
 import path from 'pathe';
-import { UtilFT } from '@zwa73/utils';
-import { ModDefine } from '@sosarciel-cdda/schema';
-
-
-
-
-
+import { GAME_MOD_DIR, ModDefine } from '@sosarciel-cdda/schema';
 
 
 
@@ -13,5 +7,4 @@ import { ModDefine } from '@sosarciel-cdda/schema';
 export const UNDef = new ModDefine("UNPC");
 export const DATA_PATH = path.join(process.cwd(),'data');
 export const ENV_PATH = path.join(process.cwd(),'..');
-export const GAME_PATH = (UtilFT.loadJSONFileSync(path.join(ENV_PATH,'build_setting.json'))! as any).game_path as string;
-export const OUT_PATH = path.join(GAME_PATH,'data','mods','UniqueNPC');
+export const OUT_PATH = path.join(GAME_MOD_DIR,'UniqueNPC');
